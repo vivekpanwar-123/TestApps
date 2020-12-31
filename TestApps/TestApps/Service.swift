92eab315222aa455f88d2b8b1424540719282291
Astha Sachdeva <asachdeva@netgear.com>
2020-12-31 14:33:00 +0530

    Finished

---
+++ TestApps/TestApps/Service.swift
@@ -0,0 +1,14 @@
+//
+//  Service.swift
+//  TestApps
+//
+//  Created by VVDN on 12/31/20.
+//
+
+import Foundation
+public class Service {
+    private init() {}
+    public static func doSomething() -> String {
+        return "My Name is Vivek Panwar"
+    }
+}
---
+++ TestApps/TestApps/TestApps.h
@@ -0,0 +1,18 @@
+//
+//  TestApps.h
+//  TestApps
+//
+//  Created by VVDN on 12/31/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestApps.
+FOUNDATION_EXPORT double TestAppsVersionNumber;
+
+//! Project version string for TestApps.
+FOUNDATION_EXPORT const unsigned char TestAppsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestApps/PublicHeader.h>
+
+
